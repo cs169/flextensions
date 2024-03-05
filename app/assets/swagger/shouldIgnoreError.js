291f15c8d14dd80d0794e60bbc8065f932ea9fab
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 03:31:41 +0000

    wip: try and find what is going on

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -4,8 +4,10 @@
  * @returns {boolean} whether or not to ignore the error.
  */
 const shouldIgnoreError = (err) => {
+    const example = err.location.match(/Structural\ error\ (.*)openapi/);
+    console.debug({ ...example });
     // This error is because the validator is still based on swagger-editor instead of swagger-editor-next.
-    if (/Structural\ error\ at\.*openapi/.test(err.location)) {
+    if (/Structural\ error\ .*openapi/.test(err.location)) {
         return true;
     }
     return false;
