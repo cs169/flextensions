85c44489371fe63f2d59e05cf1c6913e7226bed3
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 02:57:37 +0000

    wip: add more log statements

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -5,7 +5,9 @@
  */
 const shouldIgnoreError = (err) => {
     // This error is because the validator is still based on swagger-editor instead of swagger-editor-next.
+    console.debug(err);
     console.debug({ ...err });
+    console.debug(err.location);
     if (/.*Structural error at openapi.*/.test(err.location)) {
         return true;
     }
