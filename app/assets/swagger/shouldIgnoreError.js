0a789d495d0e0d2aea0d35b34784a93676d16fc0
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 02:44:08 +0000

    fix: cast error location to string

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -6,7 +6,7 @@
 const shouldIgnoreError = (err) => {
     // This error is because the validator is still based on swagger-editor instead of swagger-editor-next.
     console.debug({ ...err });
-    switch (err.location) {
+    switch (String(err.location)) {
         case 'Structural error at openapi':
             // This gets triggered from the runner using swagger-editor instead of swagger-editor-next
             return true;
