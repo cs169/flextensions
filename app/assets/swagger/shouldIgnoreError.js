f6e403a87a65c3ef50cbed132443ab8a6fc4fc79
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 02:18:37 +0000

    ci: refined scope of swagger ignored error

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -4,11 +4,16 @@
  * @returns {boolean} whether or not to ignore the error.
  */
 const shouldIgnoreError = (err) => {
-    console.debug({...err});
-    if (err.lineNo === 2) {
+    // This error is because the validator is still based on swagger-editor instead of swagger-editor next.
+    if (
+        err.location == 'Structural error at openapi' &&
+        err.message ==
+            'should match pattern "^3\\.0\\.\\d(-.+)?$"\npattern: ^3\\.0\\.\\d(-.+)?$'
+    ) {
         return true;
     }
+    // This should always be the default fallback return statement.
     return false;
-}
+};
 
-module.exports = shouldIgnoreError
+module.exports = shouldIgnoreError;
