82d9471ec628ce222a705c9f585607a2adabd9a5
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 02:31:20 +0000

    fix: ignore error message

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -4,11 +4,8 @@
  * @returns {boolean} whether or not to ignore the error.
  */
 const shouldIgnoreError = (err) => {
-    // This error is because the validator is still based on swagger-editor instead of swagger-editor next.
-    if (
-        err.location == 'Structural error at openapi' &&
-        /should match pattern "^3\\.0\\.\\d(-.+)?$".*/.test(err.message)
-    ) {
+    // This error is because the validator is still based on swagger-editor instead of swagger-editor-next.
+    if (err.location == 'Structural error at openapi') {
         return true;
     }
     // This should always be the default fallback return statement.
