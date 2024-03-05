4e7a5ecced7ca4cfaf7a359774cafc92c6f9c273
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 02:37:17 +0000

    wip: use switch statement

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -5,13 +5,14 @@
  */
 const shouldIgnoreError = (err) => {
     // This error is because the validator is still based on swagger-editor instead of swagger-editor-next.
-    console.debug({...err});
-    console.debug(err.location);
-    if (err.location == 'Structural error at openapi') {
-        return true;
+    console.debug({ ...err });
+    switch (err.location) {
+        case 'Structural error at openapi':
+            // This gets triggered from the runner using swagger-editor instead of swagger-editor-next
+            return true;
+        default:
+            return false;
     }
-    // This should always be the default fallback return statement.
-    return false;
 };
 
 module.exports = shouldIgnoreError;
