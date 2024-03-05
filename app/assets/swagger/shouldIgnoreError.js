7af8fee435ad42371b16930a2b7b76dc4bb5f34d
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 02:11:11 +0000

    fix: change shouldIgnoreError to es5 standard from es6

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -3,10 +3,12 @@
  * @param {Error} err the error being caught.
  * @returns {boolean} whether or not to ignore the error.
  */
-export default function shouldIgnoreError(err) {
+const shouldIgnoreError = (err) => {
     console.debug({...err});
     if (err.lineNo === 2) {
         return true;
     }
     return false;
 }
+
+module.exports = shouldIgnoreError
