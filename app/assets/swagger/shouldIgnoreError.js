4ac91003ff0e46423ccac6e8f22e1ba98fb89174
Connor Bernard <connorbernard@berkeley.edu>
2024-03-05 03:51:53 +0000

    docs: add comment about weird behavior

--- app/assets/swagger/shouldIgnoreError.js
+++ app/assets/swagger/shouldIgnoreError.js
@@ -4,9 +4,12 @@
  * @returns {boolean} whether or not to ignore the error.
  */
 const shouldIgnoreError = (err) => {
-    const example = err.location.match(/Structural\ error\sat\ openapi/);
-    console.debug(example);
     // This error is because the validator is still based on swagger-editor instead of swagger-editor-next.
+    /**
+     * The space after the word 'error' is not a real space, but it is converted to one when being logged
+     * out (super fun to diagnose).  It is, however, considered a whitespace character of some sort,
+     * so it can be caught by '\s'.  Don't ask how long it took to diagnose this.
+     */
     if (/Structural\ error\sat openapi/.test(err.location)) {
         return true;
     }
