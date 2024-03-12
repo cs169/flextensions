c0a7d6f7148a22b8cda1d29ce0390760e1237013
Connor Bernard <connorbernard@berkeley.edu>
2024-03-12 21:52:29 +0000

    refactor: use nested modules for namespaces

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,6 @@
-module Api::V1
-  class BaseController < Api::BaseController
+module Api
+  module V1
+    class BaseController < Api::BaseController
+    end
   end
 end
--- app/controllers/api/v1/swagger_controller.rb
+++ app/controllers/api/v1/swagger_controller.rb
@@ -1,10 +1,12 @@
 require 'json'
 
-module Api::V1
-  class SwaggerController < BaseController
-    def read
-      specFile = File.read(Rails.root + 'app/assets/swagger/swagger.json')
-      render :json => specFile
+module Api
+  module V1
+    class SwaggerController < BaseController
+      def read
+        specFile = File.read(Rails.root + 'app/assets/swagger/swagger.json')
+        render :json => specFile
+      end
     end
   end
 end
