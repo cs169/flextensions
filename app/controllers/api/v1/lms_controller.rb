69d5ebfd960148451c5bc5c0b111d72f0b90cf62
Connor Bernard <connorbernard@berkeley.edu>
2024-03-19 23:27:39 +0000

    feat: add lms name validation

--- app/controllers/api/v1/lms_controller.rb
+++ app/controllers/api/v1/lms_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class LmsController < BaseController
+      before_action :validate_name!, only: [:create]
+
       def create
         render :json => 'not yet implemented'.to_json, status: 501
       end
@@ -11,6 +13,17 @@ module Api
 
       def destroy
         render :json => 'not yet implemented'.to_json, status: 501
+      end
+
+      ##
+      # Validator definnitons.
+      # TODO: this should be exported to its own (validator) class.
+      # TODO: this validation should also check the config file for the name of lms's.
+      #
+      def validate_name!
+        if params['name'].blank?
+          render :json => 'name parameter is required', status: 401
+        end
       end
     end
   end
--- spec/controllers/api/v1/lms_controller_spec.rb
+++ spec/controllers/api/v1/lms_controller_spec.rb
@@ -2,24 +2,36 @@ require 'rails_helper'
 module Api
   module V1
     describe LmsController do
-      let(:test_course_id) { 16 }
+      let(:mock_course_id) { 16 }
+      let(:mock_course_name) { 'testCourseName' }
       describe 'create' do
         it 'throws a 501 error' do
-          post :create, params: { course_id: :test_course_id }
+          post :create, params: {
+            course_id: :mock_course_id,
+            name: :mock_course_name,
+          }
           expect(response.status).to eq(501)
+        end
+
+        it 'throws a 401 error if the name is not specified' do
+          post :create, params: {
+            course_id: :mock_course_id,
+          }
+          expect(response.status).to eq(401)
+          expect(response.body).to eq('name parameter is required')
         end
       end
 
       describe 'index' do
         it 'throws a 501 error' do
-          get :index, params: { course_id: :test_course_id }
+          get :index, params: { course_id: :mock_course_id }
           expect(response.status).to eq(501)
         end
       end
 
       describe 'destroy' do
         it 'throws a 501 error' do
-          delete :destroy, params: { course_id: :test_course_id, id: 16 }
+          delete :destroy, params: { course_id: :mock_course_id, id: 18 }
           expect(response.status).to eq(501)
         end
       end
