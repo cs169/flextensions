41be01bb6515c232013f3d3012a39945f96410b9
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-03-22 09:31:21 -0700

    Change the table name from platformcredentials to platform_credentials
    Co-authored-by: Connor <78193645+Connor-Bernard@users.noreply.github.com>

--- db/migrate/20240314230145_create_platformcredentials.rb
+++ db/migrate/20240314230145_create_platformcredentials.rb
@@ -1,6 +1,6 @@
 class CreatePlatformcredentials < ActiveRecord::Migration[7.1]
   def change
-    create_table :platformcredentials do |t|
+    create_table :platform_credentials do |t|
       t.references :user, foreign_key: true
       t.string :platform_name
       t.string :username
