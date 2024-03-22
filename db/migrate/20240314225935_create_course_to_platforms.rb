b3242313a46dd871a75b7acf915bae1eb871c035
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-03-22 09:36:54 -0700

    Change the table name from coursetoplatforms to course_to_platforms

--- db/migrate/20240314225935_create_course_to_platforms.rb
+++ db/migrate/20240314225935_create_course_to_platforms.rb
@@ -1,6 +1,6 @@
 class CreateCoursetoplatforms < ActiveRecord::Migration[7.1]
   def change
-    create_table :coursetoplatforms do |t|
+    create_table :course_to_platforms do |t|
       t.references :platform, foreign_key: true
       t.references :course, foreign_key: true
 
