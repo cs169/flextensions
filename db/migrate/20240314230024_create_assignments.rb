2c5f2b96e6bbc00724260d9951ed2b4bf89d1d54
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-03-22 09:37:59 -0700

    Change the field name from assignment_name to name
    Co-authored-by: Connor <78193645+Connor-Bernard@users.noreply.github.com>

--- db/migrate/20240314230024_create_assignments.rb
+++ db/migrate/20240314230024_create_assignments.rb
@@ -2,7 +2,7 @@ class CreateAssignments < ActiveRecord::Migration[7.1]
   def change
     create_table :assignments do |t|
       t.references :platform, foreign_key: true
-      t.string :assignment_name
+      t.string :name
 
       t.timestamps
     end
