ce317fc0a47df293d857f8e31ffd15276f7b39a2
Connor <78193645+Connor-Bernard@users.noreply.github.com>
2024-04-01 11:40:42 -0700

    [FEAT] Structure LMS Controller (#26)
    * feat: add routing
    
    * feat: configure inflections
    
    * feat: structure endpoints
    
    * test: add test coverage for new endpoints
    
    * feat: add lms name validation
    
    * docs: add swagger docs
    
    * chore: fix typo in doc comment
    
    * chore: change pluralization of lms from lms to lmss

---
+++ app/controllers/api/v1/lmss_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class LmssController < BaseController
+      before_action :validate_name!, only: [:create]
+
+      def create
+        render :json => 'not yet implemented'.to_json, status: 501
+      end
+
+      def index
+        render :json => 'not yet implemented'.to_json, status: 501
+      end
+
+      def destroy
+        render :json => 'not yet implemented'.to_json, status: 501
+      end
+
+      ##
+      # Validator definitions.
+      # TODO: this should be exported to its own (validator) class.
+      # TODO: this validation should also check the config file for the name of lms's.
+      #
+      def validate_name!
+        if params['name'].blank?
+          render :json => 'name parameter is required', status: 401
+        end
+      end
+    end
+  end
+end
