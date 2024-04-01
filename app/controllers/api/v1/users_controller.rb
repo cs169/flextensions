8333682352f1a29843e1929816aa1b232c31e6f0
Connor Bernard <connorbernard@berkeley.edu>
2024-04-01 18:34:25 +0000

    fix: pluralize files and clean up test code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,12 @@
 module Api
   module V1
     class UsersController < ApplicationController
-
       def index
             render json: 'not yet implemented', status: 501
         end
-
         def create
             render json: 'not yet implemented', status: 501
         end
-
         def destroy
             render json: 'not yet implemented', status: 501
         end
