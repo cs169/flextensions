c527246b6a3fe15a2de7beff94eb74c4fe10c87e
Zzz212zzZ <2120020201@qq.com>
2024-04-03 21:41:52 -0700

    Fix assignment controller test

--- spec/controllers/api/v1/assignments_controller_spec.rb
+++ spec/controllers/api/v1/assignments_controller_spec.rb
@@ -1,30 +1,31 @@
 require 'rails_helper'
-
 module Api
-    module V1
+  module V1
+    RSpec.describe AssignmentsController, type: :request do
+      let(:mock_course_id) { 1 }
+      let(:mock_lms_id) { 1 }
 
-        describe AssignmentsController, type: :controller do
-
-            describe "GET #index" do
-                it "throws a 501 error" do
-                    get :index
-                    expect(response.status).to eq(501)
-                end
-            end
-
-            describe "POST #create" do
-                it "throws a 501 error" do
-                    post :create
-                    expect(response.status).to eq(501)
-                end
-            end
-
-            describe "DELETE #destroy" do
-                it "throws a 501 error" do
-                    delete :destroy, params: { id: 1 }
-                    expect(response.status).to eq(501)
-                end
-            end
+      describe "GET /api/v1/courses/:course_id/lmss/:lms_id/assignments" do
+        it "throws a 501 error" do
+          get "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments"
+          expect(response).to have_http_status(501)
         end
+      end
+
+      describe "POST /api/v1/courses/:course_id/lmss/:lms_id/assignments" do
+        it "throws a 501 error" do
+          post "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments"
+          expect(response).to have_http_status(501)
+        end
+      end
+
+      describe "DELETE /api/v1/courses/:course_id/lmss/:lms_id/assignments/:id" do
+        let(:mock_assignment_id) { 1 }
+        it "throws a 501 error" do
+          delete "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments/#{mock_assignment_id}"
+          expect(response).to have_http_status(501)
+        end
+      end
     end
+  end
 end
