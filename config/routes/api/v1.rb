3fba43509e6ed7afbdde101202dcc874a430d37d
Zzz212zzZ <2120020201@qq.com>
2024-04-03 17:45:44 -0700

    Fix the route: under the courses and lmss

--- config/routes/api/v1.rb
+++ config/routes/api/v1.rb
@@ -1,6 +1,9 @@
 namespace :v1 do
   get :ping, to: 'ping#ping'
   get :swagger, to: 'swagger#read'
-  resources :assignments, only: [:index, :create, :destroy]
-  resources :courses, only: [:create, :destroy, :index]
+  resources :courses, only: [:create, :destroy, :index] do
+    resources :lmss, only: [:create, :destroy, :index] do
+      resources :assignments, only: [:index, :create, :destroy]
+    end
+  end
 end
