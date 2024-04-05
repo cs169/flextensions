4422b38842c56632ccd4ceeedeed1929eec6393b
Sepehr Behmanesh Fard <behmaneshsepehr@gmail.com>
2024-04-05 12:41:33 -0700

    fixed ordering of index and destroy

--- config/routes/api/v1.rb
+++ config/routes/api/v1.rb
@@ -1,7 +1,7 @@
 namespace :v1 do
   get :ping, to: 'ping#ping'
   get :swagger, to: 'swagger#read'
-  resources :users, only: [:index, :create, :destroy]
+  resources :users, only: [:create, :destroy, :index]
   resources :courses, only: [:create, :destroy, :index] do
     resources :lmss, only: [:create, :destroy, :index]
   end
