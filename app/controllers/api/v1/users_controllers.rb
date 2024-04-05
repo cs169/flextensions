c614d894217efc125f6da5947b14a098e4092bff
Sepehr Behmanesh Fard <behmaneshsepehr@gmail.com>
2024-04-05 12:21:36 -0700

    FEAT: Structure users controller

---
+++ app/controllers/api/v1/users_controllers.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+            render json: 'not yet implemented', status: 501
+        end
+        def create
+            render json: 'not yet implemented', status: 501
+        end
+        def destroy
+            render json: 'not yet implemented', status: 501
+        end
+    end
+  end
+end
