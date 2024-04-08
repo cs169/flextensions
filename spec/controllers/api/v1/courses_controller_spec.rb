254eb891fa773f0a69b1eb83031a9739eb10e344
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-04-08 11:20:15 -0700

    Delete tests that are included in users_controller_spec.rb
    The tests below are deleted 
    
    describe UsersController, type: :controller do
                describe 'create' do
                    it "throws a 501 error" do
                        post :create
                        expect(response.status).to eq(501)
                    end
                end
                describe 'index' do
                    it "throws a 501 error" do
                        get :index
                        expect(response.status).to eq(501)
                    end
                end
    
                describe 'destroy' do
                    it "throws a 501 error" do
                        delete :destroy, params: { id: 1 }
                        expect(response.status).to eq(501)
                    end
                end
            end

--- spec/controllers/api/v1/courses_controller_spec.rb
+++ spec/controllers/api/v1/courses_controller_spec.rb
@@ -31,29 +31,5 @@ module Api
       end
     end
 
-
-    describe UsersController do
-      describe 'create' do
-        it 'throws a 501 error' do
-          post :create
-          expect(response.status).to eq(501)
-        end
-      end
-
-      describe 'index' do
-        it 'throws a 501 error' do
-          get :index
-          expect(response.status).to eq(501)
-        end
-      end
-
-      describe 'destroy' do
-        it 'throws a 501 error' do
-          delete :destroy, params: { id: 16 }
-          expect(response.status).to eq(501)
-        end
-      end
-    end
-
   end
 end
