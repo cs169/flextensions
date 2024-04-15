f85a4ec80a3896a47890dda582f4778f4399c0bd
Evan Kandell <ekandell@berkeley.edu>
2024-04-15 15:28:36 -0700

    base seed data created

---
+++ app/models/lms.rb
@@ -0,0 +1,2 @@
+class Lms < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Assignment.destroy_all
+Lms.destroy_all
+Course.destroy_all
+User.destroy_all
+
+canvas = Lms.create!({
+    lms_name: "Canvas",
+    use_auth_token: true
+})
+
+p "Canvas ID: #{canvas.id}"
+
+
+test_assignment = Assignment.create!({
+  lms_id: canvas.id,
+  name: "Test Assignment"
+})
+
+p "Test Assignment ID: #{test_assignment.id}"
+
+
+test_course = Course.create!({
+  course_name: "Test Course",
+})
+
+p "Test Course ID: #{test_course.id}"
+
+
+test_user = User.create!({
+  email: "testuser@gmail.com",
+})
+
+p "Test User ID: #{test_user.id}"
