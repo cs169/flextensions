0e673a965845b06038d08cf61e1b5be614ae08a8
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-04-16 16:09:12 -0700

    Update users_controller.rb, fix typo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,15 +3,15 @@ module Api
     class UsersController < BaseController
 
       def create
-        render :json => 'the create method of UserController is not yet implemented'.to_json, status: 501
+        render :json => 'the create method of UsersController is not yet implemented'.to_json, status: 501
       end
   
       def index
-        render :json => 'the index method of UserController is not yet implemented'.to_json, status: 501
+        render :json => 'the index method of UsersController is not yet implemented'.to_json, status: 501
       end
   
       def destroy
-        render :json => 'the destroy method of UserController is not yet implemented'.to_json, status: 501
+        render :json => 'the destroy method of UsersController is not yet implemented'.to_json, status: 501
       end
     end
   end
