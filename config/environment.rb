6446b730ffb29406792908420f3813a059ea02f3
Connor Bernard <connorbernard@berkeley.edu>
2024-04-16 23:59:15 +0000

    feat: create canvas facade

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+ENV['CANVAS_URL'] ||= 'https://ucberkeley.test.instructure.com'
+
 # Initialize the Rails application.
 Rails.application.initialize!
