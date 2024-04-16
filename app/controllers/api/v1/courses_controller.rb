5839be948cec9fb63e5b0d3ba47e0f8928ef29e2
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-04-16 16:08:40 -0700

    Update courses_controller.rb, make the description more descriptive

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -2,19 +2,19 @@ module Api
   module V1
     class CoursesController < BaseController
       def create
-        render :json => 'not yet implemented'.to_json, status: 501
+        render :json => 'The create method of CoursesController is not yet implemented'.to_json, status: 501
       end
 
       def index
-        render :json => 'not yet implemented'.to_json, status: 501
+        render :json => 'The index method of CoursesController is not yet implemented'.to_json, status: 501
       end
 
       def destroy
-        render :json => 'not yet implemented'.to_json, status: 501
+        render :json => 'The destroy method of CoursesController is not yet implemented'.to_json, status: 501
       end
 
       def add_user
-        render :json => 'not yet implemented'.to_json, status: 501
+        render :json => 'The add_user method of CoursesControler us not yet implemented'.to_json, status: 501
       end
     end
   end
