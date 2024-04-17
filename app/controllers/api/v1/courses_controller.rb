dfc7154053fb529dda2bda6a9221b4f2b212fe18
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-17 09:50:09 -0700

    Update Gemfile

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,11 +1,26 @@
 module Api
   module V1
     class CoursesController < BaseController
+      canvas_test_base_url = "https://ucberkeley.test.instructure.com/"
+      course_id = "1534405"
       def create
         render :json => 'The create method of CoursesController is not yet implemented'.to_json, status: 501
+        # canvas_url = "{{canvas_test_base_url}}/api/v1/courses/course_id}"
+        # url = URI(canvas_url)
+        # response = Net::HTTP.get(url)
+        # course_data = JSON.parse(response)
+        # course_name = course_data['name']
+        # puts "Course Name: #{course_name}"
       end
 
       def index
+        # canvas_url = "{{canvas_test_base_url}}/api/v1/courses}"
+        # response = Net::HTTP.get(canvas_url.host, canvas_url.port)
+        # course_data = JSON.parse(response)
+        # course_data.each do |course|
+        #   course_name = course['name']
+        #   puts "Course Name: #{course_name}"
+        # end
         render :json => 'The index method of CoursesController is not yet implemented'.to_json, status: 501
       end
 
