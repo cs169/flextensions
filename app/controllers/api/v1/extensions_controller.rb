d20ff271cc376131bfb0b99125f94bd88cdc2984
zee6197 <zeeman@berkeley.edu>
2024-04-19 00:14:16 -0700

    correcting rspec for extensions controller

--- app/controllers/api/v1/extensions_controller.rb
+++ app/controllers/api/v1/extensions_controller.rb
@@ -2,42 +2,37 @@ module Api
     module V1
       class ExtensionsController < BaseController
         before_action :set_facade
-
-        # Fetch all courses the current user has access to
+  
         def index
           response = @canvas_facade.get_all_courses
           render json: response.body, status: response.status
-      end
-
-      # Create an assignment override (extension) for a student
-      def create
-        response = @canvas_facade.provision_exension(
-          params[:course_id],
-          params[:assignment_id],
-          # Array of one student ID
-          [params[:student_uid]], 
-          "Extension due to #{params[:reason]}",
-          params[:new_due_date],
-        )
-        render json: response.body, status: response.status
-      end
-
-      # Delete an existing assignment override (extension)
-      def destroy
-        response = @canvas_facade.delete_assignment_override(
-          params[:course_id],
-          params[:assignment_id],
-          params[:override_id]
-        )
-        render json: response.body, status: response.status
-      end
-
-      private
-
-      # Set up the CanvasFacade with the authorization token
-      def set_facade
-        @canvas_facade = CanvasFacade.new(request.headers['Authorization'])
+        end
+  
+        def create
+          response = @canvas_facade.provision_extension(
+            params[:course_id].to_i,
+            params[:student_uid].to_i,
+            params[:assignment_id].to_i,
+            params[:new_due_date]
+          )
+          render json: response.body, status: response.status
+        end
+  
+        def destroy
+          response = @canvas_facade.delete_assignment_override(
+            params[:course_id].to_i,
+            params[:assignment_id].to_i,
+            params[:override_id].to_i
+          )
+          render json: response.body, status: response.status
+        end
+  
+        private
+  
+        def set_facade
+          Rails.logger.info "Using CANVAS_URL: #{ENV['CANVAS_URL']}"
+          @canvas_facade = CanvasFacade.new(request.headers['Authorization'])
+        end
       end
     end
   end
-end
--- spec/controllers/api/v1/extensions_controller_spec.rb
+++ spec/controllers/api/v1/extensions_controller_spec.rb
@@ -3,47 +3,46 @@ module Api
   module V1
     RSpec.describe ExtensionsController, type: :request do
       let(:mock_course_id) { 16 }
-      let(:mock_lms_id) { 1 }
       let(:mock_assignment_id) { 9 }
       let(:mock_extension_id) { 5 }
       let(:mock_student_uid) { 123 }
-      let(:mock_reason) { 'extra time needed' }
-      let(:mock_new_due_date) { '2023-12-25' }
+      let(:mock_new_due_date) { '2024-04-16T16:00:00Z' }
 
-      # POST /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions
+      let(:auth_token) { 'Bearer some_valid_token' }
+
       describe "POST /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions" do
         context "with valid parameters" do
           it "creates a new extension and returns a success status" do
-            post "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments/#{mock_assignment_id}/extensions", params: {
-              student_uid: mock_student_uid,
-              reason: mock_reason,
-              new_due_date: mock_new_due_date
-            }, headers: { 'Authorization' => 'Bearer some_valid_token' }
+            post "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions", 
+              params: { student_uid: mock_student_uid, new_due_date: mock_new_due_date },
+              headers: { 'Authorization' => auth_token }
             expect(response).to have_http_status(:success)
           end
         end
 
         context "with invalid parameters" do
           it "returns an error status" do
-            post "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments/#{mock_assignment_id}/extensions", params: {}, headers: { 'Authorization' => 'Bearer some_valid_token' }
+            post "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions",
+              params: {}, 
+              headers: { 'Authorization' => auth_token }
             expect(response).to have_http_status(:bad_request)
           end
         end
       end
 
-      # GET /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions
       describe "GET /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions" do
         it "returns a list of extensions" do
-          get "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments/#{mock_assignment_id}/extensions", headers: { 'Authorization' => 'Bearer some_valid_token' }
+          get "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions", 
+              headers: { 'Authorization' => auth_token }
           expect(response).to have_http_status(:success)
           expect(JSON.parse(response.body)).to be_an_instance_of(Array)
         end
       end
 
-      # DELETE /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions/:id
-      describe "DELETE /api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments/#{mock_assignment_id}/extensions/:id" do
+      describe "DELETE /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions/:id" do
         it "deletes an extension and returns a success status" do
-          delete "/api/v1/courses/#{mock_course_id}/lmss/#{mock_lms_id}/assignments/#{mock_assignment_id}/extensions/#{mock_extension_id}", headers: { 'Authorization' => 'Bearer some_valid_token' }
+          delete "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions/#{mock_extension_id}", 
+                 headers: { 'Authorization' => auth_token }
           expect(response).to have_http_status(:success)
         end
       end
