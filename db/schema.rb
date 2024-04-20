99ff5e0b9696dfb110e96eee0bd2c37a721bce91
Zzz212zzZ <2120020201@qq.com>
2024-04-20 14:32:44 -0700

    Replace the external_course_id from courses table to course_to_lmss table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_20_012126) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_211809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_012126) do
     t.bigint "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "external_course_id"
     t.index ["course_id"], name: "index_course_to_lmss_on_course_id"
     t.index ["lms_id"], name: "index_course_to_lmss_on_lms_id"
   end
@@ -36,7 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_012126) do
     t.string "course_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "external_course_id"
   end
 
   create_table "extensions", force: :cascade do |t|
