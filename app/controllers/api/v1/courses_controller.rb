c10b8c0a4535102d82b1ea2b33c1852124443a9b
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-20 16:11:36 -0700

    Modified create method

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -5,26 +5,20 @@ module Api
       include ActionController::Flash
 
       def create
-        # 0. Get the course_id from the POST endpoint url
-        course_id = params[:course_id].to_i
-        # 1. Get the course from canvas
-        course_id = 1534405 # TODO: fix hardcode, this should come from the input in POST endpoint
-        canvas_base_url = Rails.application.credentials.canvas.url # https://ucberkeley.test.instructure.com
-        canvas_api_key = Rails.application.credentials.canvas.api_key
-        api = LMS::Canvas.new(canvas_base_url, canvas_api_key)
-        course_url = "#{canvas_base_url}/api/v1/courses/#{course_id}"
-        response = api.api_get_request(course_url)
-        
-        # The code in the line below outputs the response to console so that the key-value pairs can be examined
-        # puts response.inspect
+        course_name = params[:course_name]
+        existing_course = Course.find_by(course_name: course_name)
+        if existing_entry
+          render json: { messgae: 'A course with the same course name already exists.'}
+          return
+        end
 
-        # 2. Store the course in the courses table
-        course_name = response["name"]
-        @course = Course.create(course_name: course_name)
-        if @course.persisted?
-          flash[:success] = "Course created successfully"
+        new_course = Course.new(
+          course_name: course_name
+        )
+        if new_course.save
+          render json: new_course, status: :created
         else
-          flash.now[:error] = "Failed to create course"
+          render json: new_course.errors, status: :unprocessable_entity
         end
       end
 
