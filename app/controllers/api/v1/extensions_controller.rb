274fc937065fd6637568f7b11275ea634b18de63
Evan Kandell <ekandell@berkeley.edu>
2024-04-20 14:54:19 -0700

    More detailed extensions req, still needs more

--- app/controllers/api/v1/extensions_controller.rb
+++ app/controllers/api/v1/extensions_controller.rb
@@ -9,13 +9,35 @@ module Api
         end
   
         def create
+          @course = Course.find(params[:course_id])
+          @assignment = Assignment.find(params[:assignment_id])
           response = @canvas_facade.provision_extension(
-            params[:course_id].to_i,
+            @course.external_course_id.to_i,
             params[:student_uid].to_i,
-            params[:assignment_id].to_i,
+            @assignment.external_assignment_id.to_i,
             params[:new_due_date]
           )
-          render json: response.body, status: response.status
+
+          if (response.status == 200)
+            assignment_override = response.body
+            # if request succeeds, create a new Extension object
+            @extension = Extension.new(
+            assignment_id: @assignment.id # foreign key to local assignment
+            student_email: nil # requires another api request to find student data (sid is given in first response)
+            initial_due_date: nil # also requires an api request to find assignment data (assignment id is given in first response)
+            new_due_date: assignment_override["due_at"]
+            external_extension_id: assignment_override["id"]
+            )
+            if @extension.save
+              render json: @extension.to_json, status: 200
+            else
+              render status: 500
+            end
+          else
+            render json: response.body, status: response.status
+          end
+
+
         end
   
         def destroy
