569dc2e79c00fb3a594bee60d997e1bbe0e23c0a
Zzz212zzZ <2120020201@qq.com>
2024-04-20 17:44:33 -0700

    Refactor to pass the Codeclimate

--- app/controllers/api/v1/assignments_controller.rb
+++ app/controllers/api/v1/assignments_controller.rb
@@ -9,51 +9,51 @@ module Api
 
       # POST /courses/:course_id/lms/:lms_id/assignments
       def create
-        assignment_name = params[:name]
-        external_assignment_id = params[:external_assignment_id]
-        course_id = params[:course_id]
-        lms_id = params[:lms_id]
+        course_to_lms = fetch_course_to_lms(params[:course_id], params[:lms_id])
+        return unless course_to_lms
 
-        # Retrieve the course_to_lms entry
-        course_to_lms = CourseToLms.find_by(course_id: course_id, lms_id: lms_id)
+        return if assignment_exists?(course_to_lms, params[:name], params[:external_assignment_id])
 
-        unless course_to_lms
-          render json: { error: 'Course to LMS association not found' }, status: :not_found
-          return
-        end
-
-        # Check this assignment doesn't already exist
-        existing_assignment = Assignment.find_by(course_to_lms_id: course_to_lms.id, name: assignment_name, external_assignment_id: external_assignment_id)
-        if existing_assignment
-          render json: { message: 'The assignment with the specified external ID already exists.' }, status: :ok
-          return
-        end
-
-        # Create the assignment
-        assignment = Assignment.new(
-          course_to_lms_id: course_to_lms.id,
-          name: assignment_name,
-          external_assignment_id: external_assignment_id
-        )
-
-        if assignment.save
-          render json: assignment, status: :created
-        else
-          render json: assignment.errors, status: :unprocessable_entity
-        end
-
+        create_and_render_assignment(course_to_lms, params[:name], params[:external_assignment_id])
       end
 
       def destroy
         render json: 'not yet implemented', status: 501
       end
 
+      private
+
+      def fetch_course_to_lms(course_id, lms_id)
+        course_to_lms = CourseToLms.find_by(course_id: course_id, lms_id: lms_id)
+        unless course_to_lms
+          render json: { error: 'No such Course_LMS association' }, status: :not_found
+        end
+        course_to_lms
+      end
+
+      def assignment_exists?(course_to_lms, assignment_name, external_assignment_id)
+        existing_assignment = Assignment.find_by(course_to_lms_id: course_to_lms.id, name: assignment_name, external_assignment_id: external_assignment_id)
+        if existing_assignment
+          render json: { message: 'Record already exists' }, status: :ok
+          return true
+        end
+        false
+      end
+
+      def create_and_render_assignment(course_to_lms, assignment_name, external_assignment_id)
+        assignment = Assignment.new(course_to_lms_id: course_to_lms.id, name: assignment_name, external_assignment_id: external_assignment_id)
+        if assignment.save
+          render json: assignment, status: :created
+        else
+          render json: assignment.errors, status: :unprocessable_entity
+        end
+      end
+
       def validate_ids!
         if params[:name].blank? || params[:external_assignment_id].blank? || params[:course_id].blank? || params[:lms_id].blank?
-          render json: { error: 'Course ID, LMS ID, name, and external assignment ID are required' }, status: :bad_request
+          render json: { error: 'Params required' }, status: :bad_request
         end
       end
     end
   end
 end
-  
