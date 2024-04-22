4ee7e63f10e41bd8ef6ebcfa81760f753e85a9dc
Sepehr Behmanesh Fard <behmaneshsepehr@gmail.com>
2024-04-22 16:17:10 -0700

    validation email added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # app/models/user.rb
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
 
     # Associasions
     has_many :lms_credentials, dependent: :destroy
