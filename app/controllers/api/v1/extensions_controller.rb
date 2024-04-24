0f19af82b37e1650de8f81e805e88b249ed9e6a1
Evan Kandell <ekandell@berkeley.edu>
2024-04-24 01:22:51 -0700

    tests pass. finally.

--- app/controllers/api/v1/extensions_controller.rb
+++ app/controllers/api/v1/extensions_controller.rb
@@ -10,7 +10,7 @@ module Api
         end
   
         def create
-          @lms = Lms.where(lms_name: "canvas")
+          @lms = Lms.where(lms_name: "Canvas").take
           @course = Course.find(params[:course_id])
           @assignment = Assignment.find(params[:assignment_id])
           @course_to_lms = CourseToLms.where(lms_id: @lms.id, course_id: @course.id).take
@@ -22,20 +22,22 @@ module Api
           )
 
           if (response.status == 200) 
-            assignment_override = response.body
+            assignment_override = JSON.parse(response.body)
             # if request succeeds, create a new Extension object
             assignment_response = @canvas_facade.get_assignment(@course_to_lms.external_course_id.to_i, @assignment.external_assignment_id.to_i)
             if (assignment_response.status != 200)
               render json: assignment_response.to_json, status: 500
               return
             end
+            assignment_json = JSON.parse(assignment_response.body)
             @extension = Extension.new(
             assignment_id: @assignment.id, # foreign key to local assignment
-            student_email: nil, # requires another api request to find student data (sid is given in first response)
-            initial_due_date: assignment_response.body["due_at"], # also requires an api request to find assignment data (assignment id is given in first response)
+            student_email: nil, # requires another api request to find student data (sid is given in first response). This currently doesn't exist in CanvasFacade
+            initial_due_date: assignment_json["due_at"], # also requires an api request to find assignment data (assignment id is given in first response)
             #Note that the assignment.due_at shows the due date as it is for whoever's logged in (which if it's a teacher, should be the original due date) but the actual original due date is never saved anywhere
             new_due_date: assignment_override["due_at"],
-            external_extension_id: assignment_override["id"]
+            external_extension_id: assignment_override["id"],
+            last_processed_by_id: nil # Requires login/sessions to be properly implemented
             )
             if @extension.save
               render json: @extension.to_json, status: 200
--- spec/controllers/api/v1/extensions_controller_spec.rb
+++ spec/controllers/api/v1/extensions_controller_spec.rb
@@ -1,44 +1,59 @@
 require 'rails_helper'
+require 'byebug'
 module Api
   module V1
-    RSpec.describe ExtensionsController, type: :request do
-      let(:mock_course_id) { 16 }
-      let(:mock_assignment_id) { 9 }
-      let(:mock_extension_id) { 5 }
-      let(:mock_student_uid) { 123 }
-      let(:mock_new_due_date) { '2024-04-16T16:00:00Z' }
-
-      let(:auth_token) { 'Bearer some_valid_token' }
-
-
-
+    RSpec.describe ExtensionsController do
 
 
       describe "POST /api/v1/courses/:course_id/lmss/:lms_id/assignments/:assignment_id/extensions" do
-        context "with valid parameters" do
-          it "creates a new extension and returns a success status" do
 
+        before(:all) do
+          load "#{Rails.root}/db/seeds.rb" 
+          @course =  Course.take
+          @assignment = Assignment.take
+          @extension = Extension.take
+          @lms = Lms.take
+          @course_to_lms = CourseToLms.where(lms_id: @lms.id, course_id: @course.id).take
+          @mock_student_uid = 123
+          @mock_new_due_date = '2024-04-16T16:00:00Z'
+
+          @auth_token = 'some_valid_token'
+
+          puts @course.id
+        end
+
+        context "with valid parameters" do
+
+          it "creates a new extension and returns a success status" do
+  
             # stub proper extension request
-            stub_request(:post, "#{ENV['CANVAS_URL']}/api/v1/courses/#{mock_course_id}/assignments/#{mock_assignment_id}/overrides").
+            stub_request(:post, "#{ENV['CANVAS_URL']}/api/v1/courses/#{@course_to_lms.external_course_id}/assignments/#{@assignment.external_assignment_id}/overrides").
          with(
-           body: hash_including({"due_at"=>"#{mock_new_due_date}", "lock_at"=>"#{mock_new_due_date}", "student_ids"=>["#{mock_student_uid}"],
-            "title"=>"#{mock_student_uid} extended to #{mock_new_due_date}"}),
+           body: hash_including({"due_at"=>"#{@mock_new_due_date}", "lock_at"=>"#{@mock_new_due_date}", "student_ids"=>["#{@mock_student_uid}"],
+            "title"=>"#{@mock_student_uid} extended to #{@mock_new_due_date}"}),
+            headers: {"Authorization"=> "Bearer #{@auth_token}"}
            ).
-         to_return(status: 200, body: "", headers: {})
-            post "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions", 
-              params: { student_uid: mock_student_uid, new_due_date: mock_new_due_date },
-              headers: { 'Authorization' => auth_token }
+          to_return(status: 200, body: {"due_at"=>@mock_new_due_date, "id"=>"3333"}.to_json, headers: {})
+
+          stub_request(:get, "#{ENV['CANVAS_URL']}/api/v1/courses/#{@course_to_lms.external_course_id}/assignments/#{@assignment.external_assignment_id}")
+            .to_return(status: 200, body: {"due_at"=> '2024-04-13T16:00:00Z'}.to_json, headers: {})
+            
+          request.headers.merge!({'Authorization': @auth_token})
+          post :create, params: {
+              "course_id"=> @course.id, "lms_id"=> @lms.id, "assignment_id"=> @assignment.id, "student_uid"=>@mock_student_uid, "new_due_date"=>@mock_new_due_date
+            }
+            #post "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions", params: { student_uid: mock_student_uid, new_due_date: mock_new_due_date }#, headers: { 'Authorization' => auth_token }
             expect(response).to have_http_status(:success)
+            puts response.body
           end
         end
 
         context "with invalid parameters" do
           it "returns an error status" do
-            stub_request(:post, "#{ENV['CANVAS_URL']}/api/v1/courses/#{mock_course_id}/assignments/#{mock_assignment_id}/overrides").
+            stub_request(:post, "#{ENV['CANVAS_URL']}/api/v1/courses/#{@course_to_lms.external_course_id}/assignments/#{@assignment.external_assignment_id}/overrides").
             to_return(status: 400)
-            expect {post "/api/v1/courses/#{mock_course_id}/lmss/1/assignments/#{mock_assignment_id}/extensions",
-              params: {}, 
-              headers: { 'Authorization' => auth_token }
+            expect {post :create,
+              params: {"course_id"=> @course.id, "lms_id"=> @lms.id, "assignment_id"=> @assignment.id}
           }.to raise_error(FailedPipelineError)
           end
         end
