e9162bb2fb80025a309bbebe3d914f8283bd9aca
Cynthia Xinyi Li <76521916+cynthia-lixinyi@users.noreply.github.com>
2024-04-24 09:18:23 -0700

    Implemented add_user in courses controller (#44)
    * Implemented add_user in courses controller
    
    * Refactor code to eliminate repetitive code
    
    * Update user_to_course.rb

--- app/models/user_to_course.rb
+++ app/models/user_to_course.rb
@@ -1,4 +1,10 @@
 class UserToCourse < ApplicationRecord
-    belongs_to :user
-    belongs_to :course
+  # Associations
+  belongs_to :user
+  belongs_to :course
+
+  # Validations
+  validates :user_id, presence: true
+  validates :course_id, presence: true
+  validates :role, presence: true
 end
