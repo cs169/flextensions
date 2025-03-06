52b32f3be5ee6c47143d52584aa3ba1a362f1ae8
root <root@Tianye>
2025-03-06 15:36:47 -0800

    removed unrelated code piceds

--- app/controllers/Login_controller.rb
+++ app/controllers/Login_controller.rb
@@ -27,30 +27,4 @@ class LoginController < ApplicationController
         session[:user_id] = nil
         redirect_to root_path
     end
-
-
-    def create_session(create_if_not_exists)
-        user_info = request.env['omniauth.auth']
-        user = find_or_create_user(user_info, create_if_not_exists)
-        session[:current_user_id] = user.uid
-        destination_url = session[:destination_after_login] || root_url
-        session[:destination_after_login] = nil
-        redirect_to destination_url
-    end
-
-    def find_or_create_user(user_info, create_if_not_exists)
-        user = User.find_by(
-            uid:      user_info['uid']
-        )
-        return user unless user.nil?
-        send(create_if_not_exists, user_info)
-    end
-
-    def create_canvas_user(user_info)
-        User.create(
-            uid:        user_info['uid'],
-            email:      user_info['email']
-        )
-    end
-
   end
