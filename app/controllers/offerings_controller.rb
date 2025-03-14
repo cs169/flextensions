6484e67dddded6a4cbc48d624b63cdb70fbbe223
Eric Huang <huanger@berkeley.edu>
2025-03-14 15:24:06 -0700

    fix rubocop errors

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -11,27 +11,27 @@ class OfferingsController < ApplicationController
     token = user.canvas_token
     @courses = fetch_courses(token)
     if @courses.empty?
-      Rails.logger.info "No courses found."
-      flash[:alert] = "No courses found."
+      Rails.logger.info 'No courses found.'
+      flash[:alert] = 'No courses found.'
     end
-    #Rails.logger.info "Courses: #{@courses}"
+    # Rails.logger.info "Courses: #{@courses}"
+    teacher_roles = %w[teacher ta]
     @courses_teacher = @courses.select do |course|
-      course["enrollments"].any? { |enrollment| ["teacher", "ta"].include?(enrollment["type"]) }
+      course['enrollments'].any? { |enrollment| teacher_roles.include?(enrollment['type']) }
     end
 
     @courses_student = @courses.select do |course|
-      course["enrollments"].any? { |enrollment| enrollment["type"] == "student" }
+      course['enrollments'].any? { |enrollment| enrollment['type'] == 'student' }
     end
-
   end
 
   private
 
   def fetch_courses(token)
-    response = Faraday.get(ENV['CANVAS_URL'] + "/api/v1/courses") do |req|
+    response = Faraday.get("#{ENV.fetch('CANVAS_URL', nil)}/api/v1/courses") do |req|
       req.headers['Authorization'] = "Bearer #{token}"
-      req.headers['Content-Type'] = "application/json"
-      req.params['include[]'] = "enrollment_type"
+      req.headers['Content-Type'] = 'application/json'
+      req.params['include[]'] = 'enrollment_type'
     end
 
     if response.success?
@@ -41,5 +41,4 @@ class OfferingsController < ApplicationController
       []
     end
   end
-
 end
