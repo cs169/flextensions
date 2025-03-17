8320933b4879101fa9d479ea82cfccb4dca24ea2
root <root@Tianye>
2025-03-17 10:59:17 -0700

    Logout and user token added back due to missing master key

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,13 +9,13 @@ class SessionController < ApplicationController
     token = get_access_token(canvas_code)
     # Fetch user profile from Canvas API using the token
     response = Faraday.get("#{ENV.fetch('CANVAS_URL', nil)}/api/v1/users/self?") do |req|
-      req.headers['Authorization'] = "Bearer #{token}"
+      req.headers['Authorization'] = "Bearer #{token.token}"
     end
 
     if response.success?
       user_data = JSON.parse(response.body)
-      Rails.logger.debug(user_data)
-      Rails.logger.info "User Data: #{user_data}"
+    #   Rails.logger.debug(user_data)
+    #   Rails.logger.info "User Data: #{user_data}"
       find_or_create_user(user_data, token)
       redirect_to offerings_path, notice: 'Logged in!'
     else
@@ -24,12 +24,15 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out!'
+    redirect_to :logout, notice: 'Logged out!'
   end
 
   private
-
+  
+  # Everytime someone tries to log in, they have to get a new token.
+  # There is no way we reuse the token for login since when a user clicks
+  # the login button, they are redirected to the Canvas login page immediately. 
+  # If you are looking for logic of refresh token, 
   def get_access_token(code)
     client = OAuth2::Client.new(
       ENV.fetch('CANVAS_CLIENT_ID', nil),
@@ -38,31 +41,52 @@ class SessionController < ApplicationController
       token_url: '/login/oauth2/token'
     )
     token = client.auth_code.get_token(code, redirect_uri: :canvas_callback)
-    token.token
+    token
   end
 
-  def find_or_create_user(user_data, token)
+  def find_or_create_user(user_data, full_token)
     # Find or create user in database
-    Rails.logger.info "User token: #{token}"
-
+    token = full_token.token
     user = nil
     if User.exists?(email: user_data['primary_email'])
       user = User.find_by(email: user_data['primary_email'])
-      user.update(canvas_token: token)
+      user.canvas_uid = user_data['id']
+      user.canvas_token = token
+      user.save!
     elsif User.exists?(canvas_uid: user_data['id'])
       user = User.find_by(canvas_uid: user_data['id'])
-      user.update(canvas_token: token)
+      user.email = user_data['email']
+      user.canvas_token = token
+      user.save!
     else
       user = User.find_or_initialize_by(canvas_uid: user_data['id'])
       user.assign_attributes(
         email: user_data['email'],
         name: user_data['name'],
-        canvas_token: token
       )
+      user.canvas_token = token
       user.save!
     end
+    #update user's lms credentials.
+    # if user.lms_credentials.any?
+    #     user.lms_credentials.first.update(
+    #         token: token.token,
+    #         refresh_token: token.refresh_token,
+    #         expire_time: Time.at(token.expires_at)
+    #         )
+    # else 
+    #     user.lms_credentials.create!(
+    #         user_id: user.canvas_uid,
+    #         lms_name: 'canvas',
+    #         token: token,
+    #         refresh_token: full_token.refresh_token,
+    #         expire_time: Time.at(full_token.expires_at)
+    #     )
+        
+    # end
     # Store user ID in session for authentication
     session[:username] = user.name
-    session[:user_id] = user.id
+    session[:user_id] = user.canvas_uid
   end
+
 end
