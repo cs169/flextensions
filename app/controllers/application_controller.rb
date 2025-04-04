3744878d019da04a77bc6c058dd603fdc8c1b25b
root <root@Tianye>
2025-04-04 12:56:02 -0700

    added flash message when redirecting back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,31 +26,30 @@ class ApplicationController < ActionController::Base
       @current_user = User.find_by(canvas_uid: session[:user_id])
       if @current_user.nil?
         # User is not found in the database
-        puts('User not found in the database.')
+        flash[:alert] = 'User not found in the database.'
         redirect_to root_path#, flash: 'User not found.'
         return false
       elsif @current_user.lms_credentials.empty?
-        puts('User has no credentials.')
+        flash[:alert] = 'User has no credentials.'
         redirect_to root_path#, flash: 'Invalid user credentials.'
         return false
       elsif @current_user.lms_credentials.first.expire_time < Time.zone.now
-        puts(@current_user.lms_credentials.first.expire_time)
-        puts(Time.zone.now)
-        puts('User token has expired.')
+        flash[:alert] = 'User token has expired.'
         # User's token has expired
         redirect_to root_path#, flash: 'Your session has expired. Please log in again.'
         return false
       elsif @current_user.lms_credentials.first.expire_time > Time.zone.now
-        puts('User token is still valid.')
+        flash[:alert] = 'User token is still valid.'
         # User's token is still valid
         return true
       else
         # Unhandled cases
-        puts('An unexpected error occurred.')
+        flash[:alert] = 'An unexpected error occurred.'
         redirect_to root_path#, flash: 'An unexpected error occurred.'
         return false
       end
     end
-    redirect_to root_path, alert: 'Please log in to access this page.'
+    flash[:alert] = 'Please log in to access this page.'
+    redirect_to root_path
   end
 end
