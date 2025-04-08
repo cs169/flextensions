c1ed23d424b84aa8f7b46b4e577755109a5052b8
root <root@Tianye>
2025-04-08 13:24:55 -0700

    rubocop branch factor fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,39 +19,23 @@ class ApplicationController < ActionController::Base
 
   # This method checks if the user has loggedin and has valid credentials.
   def authenticated!
-    unless session[:user_id].nil?
-      # User has logged in
-      return true if Rails.env.test?
+    return true if session[:user_id].present? && Rails.env.test?
 
-      @current_user = User.find_by(canvas_uid: session[:user_id])
-      if @current_user.nil?
-        # User is not found in the database
-        flash[:alert] = 'User not found in the database.'
-        redirect_to root_path # , flash: 'User not found.'
-        return false
-      end
-      if @current_user.lms_credentials.empty?
-        flash[:alert] = 'User has no credentials.'
-        redirect_to root_path # , flash: 'Invalid user credentials.'
-        return false
-      end
-      if @current_user.lms_credentials.first.expire_time < Time.zone.now
-        flash[:alert] = 'User token has expired.'
-        # User's token has expired
-        redirect_to root_path # , flash: 'Your session has expired. Please log in again.'
-        return false
-      end
-      if @current_user.lms_credentials.first.expire_time > Time.zone.now
-        flash[:alert] = 'User token is still valid.'
-        # User's token is still valid
-        return true
-      end
-      # Unhandled cases
-      flash[:alert] = 'An unexpected error occurred.'
-      redirect_to root_path # , flash: 'An unexpected error occurred.'
-      return false
-    end
-    flash[:alert] = 'Please log in first to access any other pages.'
+    @current_user = User.find_by(canvas_uid: session[:user_id])
+    return handle_authentication_failure('User not found in the database.') if @current_user.nil?
+    return handle_authentication_failure('User has no credentials.') if @current_user.lms_credentials.empty?
+
+    token_expiry_time = @current_user.lms_credentials.first.expire_time
+    return handle_authentication_failure('User token has expired.') if token_expiry_time < Time.zone.now
+
+    return true if token_expiry_time > Time.zone.now
+
+    handle_authentication_failure('An unexpected error occurred.')
+  end
+
+  def handle_authentication_failure(message)
+    flash[:alert] = message
     redirect_to root_path
+    false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2025_04_08_015944) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
