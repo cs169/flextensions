8c69aaff596b1d57c3640945c1ec59d626bc7778
Tashrique Ahmed <ta13@williams.edu>
2025-04-10 00:41:02 -0700

    controller actions for testing fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :log_session_info
   before_action :authenticated!, unless: -> { excluded_controller_action? }
 
   def excluded_controller_action?
@@ -19,34 +18,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # This method checks if the user has loggedin and has valid credentials.
   def authenticated!
-    unless session[:user_id].nil?
-      # User has logged in
-      return true if Rails.env.test?
+    return unless session[:user_id].nil?
 
-      @current_user = User.find_by(canvas_uid: session[:user_id])
-      if @current_user.nil?
-        # User is not found in the database
-        redirect_to root_path, flash: 'User not found.'
-        return false
-      elsif @current_user.lms_credentials.empty?
-        redirect_to root_path, flash: 'Invalid user credentials.'
-        # User has no credentials
-        return false
-      elsif @current_user.lms_credentials.first.expire_time < Time.zone.now
-        # User's token has expired
-        redirect_to root_path, flash: 'Your session has expired. Please log in again.'
-        return false
-      elsif @current_user.lms_credentials.first.expire_time > Time.zone.now
-        # User's token is still valid
-        return true
-      else
-        # Unhandled cases
-        redirect_to root_path, flash: 'An unexpected error occurred.'
-        return false
-      end
-    end
+    redirect_to root_path, alert: 'Please log in to access this page.'
+  end
+
+  def authenticated!
+    return unless session[:user_id].nil?
+
     redirect_to root_path, alert: 'Please log in to access this page.'
   end
 end
