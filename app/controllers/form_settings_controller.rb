061264136a07cf0bb64665ae35ccad0212b7629b
Yaman Tarakji <yamyoume@hotmail.com>
2025-04-13 02:48:21 -0700

    made the update stay in form_settings instead of redirecting to assignments

--- app/controllers/form_settings_controller.rb
+++ app/controllers/form_settings_controller.rb
@@ -14,7 +14,8 @@ class FormSettingsController < ApplicationController
   def update
     @form_setting = @course.form_setting || @course.build_form_setting
     if @form_setting.update(form_setting_params)
-      redirect_to course_path(@course), notice: 'Form settings updated successfully.'
+      flash.now[:notice] = 'Form settings updated successfully.'
+      render :edit
     else
       render :edit
     end
