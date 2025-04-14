fc18ba502a48e0a890c8541bb89855783d1f3ce9
Tashrique Ahmed <ta13@williams.edu>
2025-04-13 21:05:35 -0700

    Fixed schema

--- db/migrate/20250414013026_remove_custom_questions_from_course_settings.rb
+++
@@ -1,7 +0,0 @@
-class RemoveCustomQuestionsFromCourseSettings < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :course_settings, :custom_question_1, :text
-    remove_column :course_settings, :custom_question_2, :text
-    remove_column :course_settings, :custom_question_3, :text
-  end
-end
