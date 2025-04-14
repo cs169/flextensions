8cc5680e606e804d20089fd8ab2f64a705054d61
Tashrique Ahmed <ta13@williams.edu>
2025-04-13 18:03:22 -0700

    Assignments boolean removed

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,24 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_12_235327) do
     t.bigint "course_to_lms_id", null: false
     t.datetime "due_date"
     t.datetime "late_due_date"
-    t.boolean "extensions_enabled"
     t.boolean "enabled", default: false
+  end
+
+  create_table "course_settings", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.boolean "enable_student_requests", default: false
+    t.integer "auto_approve_days"
+    t.integer "auto_approve_dsp_days"
+    t.integer "max_auto_approve"
+    t.string "reply_email"
+    t.text "custom_question_1"
+    t.text "custom_question_2"
+    t.text "custom_question_3"
+    t.string "email_subject"
+    t.text "email_template"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_course_settings_on_course_id"
   end
 
   create_table "course_to_lmss", force: :cascade do |t|
@@ -122,6 +138,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_12_235327) do
   end
 
   add_foreign_key "assignments", "course_to_lmss"
+  add_foreign_key "course_settings", "courses"
   add_foreign_key "course_to_lmss", "courses"
   add_foreign_key "course_to_lmss", "lmss"
   add_foreign_key "extensions", "assignments"
