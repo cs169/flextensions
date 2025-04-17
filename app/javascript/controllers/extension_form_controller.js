230caa63a9debe5dd9d50bf3b5860ed1f89f2f0f
Eric Huang <huanger@berkeley.edu>
2025-04-17 00:18:56 -0700

    revert js code

--- app/javascript/controllers/extension_form_controller.js
+++ app/javascript/controllers/extension_form_controller.js
@@ -5,71 +5,58 @@ export default class extends Controller {
   static targets = ["daysDisplay", "dueDateInput", "assignmentSelect"]
 
   connect() {
-    if (this._isUpdateOnlyMode()) {
-      this._setupUpdateOnlyMode();
+    // Check if we're in update-only mode (e.g. in edit view)
+    if (this.assignmentSelectTarget.dataset.updateOnly === "true") {
+      // Get the original due date string either from data or from the input value.
+      this.originalDueDateStr = this.dueDateInputTarget.dataset.originalDueDate || document.getElementById("edit-request-requested-due-date").value;
+      // Expect the value in update-only mode to be in "YYYY-MM-DD" format.
+      this.originalDueDate = new Date(this.originalDueDateStr);
+      
+      // Set the minimum requested date to be one day after the original due date.
+      const minDate = new Date(this.originalDueDate);
+      minDate.setDate(minDate.getDate() + 1);
+      this.dueDateInputTarget.min = this._toDateString(minDate);
+      // If the current input value is older than the minDate, update it.
+      let currentDueDate = new Date(this.dueDateInputTarget.value);
+      if (currentDueDate < minDate) {
+        this.dueDateInputTarget.value = this._toDateString(minDate);
+      }
+      
+      // Attach event listeners.
+      this.dueDateInputTarget.addEventListener("input", () => this._updateDays());
+      this.assignmentSelectTarget.addEventListener("change", () => this.updateAssignment());
+      this._updateDays();
+      return;
     } else {
-      this._setupNormalMode();
+      // Normal behavior for new view.
+      this.originalDueDateStr = this.dueDateInputTarget.dataset.originalDueDate;
+      if (this.originalDueDateStr && this.originalDueDateStr.includes(" at ")) {
+        this.originalDueDate = new Date(this.originalDueDateStr.split(" at ")[0]);
+      } else {
+        this.originalDueDateStr = this.dueDateInputTarget.value;
+        this.originalDueDate = new Date(this.originalDueDateStr);
+      }
     }
-
+  
     if (this.originalDueDateStr) {
-      this._setMinAndCurrentDueDate();
+      // In normal mode, add one day to set the minimum requested due date.
+      const minDate = new Date(this.originalDueDateStr.split(" at ")[0]);
+      minDate.setDate(minDate.getDate() + 1); // add one day
+      this.dueDateInputTarget.min = this._toDateString(minDate);
+      this.dueDateInputTarget.value = this._toDateString(minDate);
+  
+      // Ensure the input value is at least the min date.
+      const currentDueDate = new Date(this.dueDateInputTarget.value);
+      if (currentDueDate < minDate) {
+        this.dueDateInputTarget.value = this._toDateString(minDate);
+      }
+  
       this._updateDays();
     }
-
-    this._attachEventListeners();
-  }
-
-  _isUpdateOnlyMode() {
-    return this.assignmentSelectTarget.dataset.updateOnly === "true";
-  }
-
-  _setupUpdateOnlyMode() {
-    this.originalDueDateStr = this.dueDateInputTarget.dataset.originalDueDate || 
-      document.getElementById("edit-request-requested-due-date").value;
-    this.originalDueDate = new Date(this.originalDueDateStr);
-
-    const minDate = this._getMinDate(this.originalDueDate);
-    this.dueDateInputTarget.min = this._toDateString(minDate);
-
-    const currentDueDate = new Date(this.dueDateInputTarget.value);
-    if (currentDueDate < minDate) {
-      this.dueDateInputTarget.value = this._toDateString(minDate);
-    }
-
-    this._attachUpdateOnlyListeners();
-    this._updateDays();
-  }
-
-  _setupNormalMode() {
-    this.originalDueDateStr = this.dueDateInputTarget.dataset.originalDueDate || this.dueDateInputTarget.value;
-    this.originalDueDate = new Date(this.originalDueDateStr.split(" at ")[0]);
-  }
-
-  _setMinAndCurrentDueDate() {
-    const minDate = this._getMinDate(this.originalDueDate);
-    this.dueDateInputTarget.min = this._toDateString(minDate);
-    this.dueDateInputTarget.value = this._toDateString(minDate);
-
-    const currentDueDate = new Date(this.dueDateInputTarget.value);
-    if (currentDueDate < minDate) {
-      this.dueDateInputTarget.value = this._toDateString(minDate);
-    }
-  }
-
-  _attachEventListeners() {
+  
+    // Attach listeners for any further changes.
     this.dueDateInputTarget.addEventListener("input", () => this._updateDays());
     this.assignmentSelectTarget.addEventListener("change", () => this.updateAssignment());
-  }
-
-  _attachUpdateOnlyListeners() {
-    this.dueDateInputTarget.addEventListener("input", () => this._updateDays());
-    this.assignmentSelectTarget.addEventListener("change", () => this.updateAssignment());
-  }
-
-  _getMinDate(date) {
-    const minDate = new Date(date);
-    minDate.setDate(minDate.getDate() + 1);
-    return minDate;
   }
 
   // Public method so that the action "input->extension-form#updateDays" works.
