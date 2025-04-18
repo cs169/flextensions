8205bf654ee4c51416f6d008bc3a23207ceb4873
Eric Huang <huanger@berkeley.edu>
2025-04-17 23:26:21 -0700

    support instructor requests by oldest first and student requests by newest first

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,7 +6,7 @@ class RequestsController < ApplicationController
 
   def index
     @side_nav = 'requests'
-    @requests = @role == 'student' ? @course.requests.for_user(@user).order(created_at: :desc) : @course.requests.includes(:assignment).order(created_at: :desc)
+    @requests = @role == 'student' ? @course.requests.for_user(@user).order(created_at: :desc) : @course.requests.includes(:assignment).order(created_at: :asc)
     render_role_based_view
   end
 
