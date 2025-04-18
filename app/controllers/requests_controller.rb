a2cbcdfa807de807c655dbe30dba7d47e7b003cf
Eric Huang <huanger@berkeley.edu>
2025-04-17 19:14:20 -0700

    fix typo

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -132,7 +132,7 @@ class RequestsController < ApplicationController
       )
 
       if extension.save
-        @request.update(stats: 'approved', external_extension_id: assignment_override['id'])
+        @request.update(status: 'approved', external_extension_id: assignment_override['id'])
         redirect_to course_requests_path(@course), notice: 'Request accepted and extension created successfully in Canvas.'
       else
         redirect_to course_requests_path(@course), alert: 'Extension created in Canvas, but failed to save locally.'
