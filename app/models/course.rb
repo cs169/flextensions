ab7be457fb824e1750537d7866abebb48770cefe
Eric Huang <huanger@berkeley.edu>
2025-04-19 00:36:34 -0700

    i dunno man

--- app/models/course.rb
+++ app/models/course.rb
@@ -100,12 +100,12 @@ class Course < ApplicationRecord
 
   # Sync a single assignment
   def self.sync_assignment(course_to_lms, assignment_data)
-    Rails.logger.info "#assignment_data: #{assignment_data}"
+    # Rails.logger.info "#assignment_data: #{assignment_data}"
     assignment = Assignment.find_or_initialize_by(course_to_lms_id: course_to_lms.id, external_assignment_id: assignment_data['id'])
     assignment.name = assignment_data['name']
     assignment.due_date = DateTime.parse(assignment_data['due_at']) if assignment_data['due_at'].present?
-    Rails.logger.info "Assignment due date: #{assignment.due_date}"
-    assignment.late_due_date = DateTime.parse(assignment_data['due_at']) if assignment_data['due_at'].present? && (assignment.late_due_date.nil? || assignment.late_due_date < DateTime.parse(assignment_data['due_at']))
+    # Rails.logger.info "Assignment due date: #{assignment.due_date}"
+    assignment.late_due_date = DateTime.parse(assignment_data['lock_at']) if assignment_data['lock_at'].present? && (assignment.late_due_date.nil? || assignment.late_due_date < DateTime.parse(assignment_data['lock_at']))
     assignment.save!
   end
 
