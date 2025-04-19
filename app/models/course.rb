97fba6e5852b2fb4bd5cc591cde35eaab65d614e
Eric Huang <huanger@berkeley.edu>
2025-04-19 00:48:07 -0700

    removed rails loggers

--- app/models/course.rb
+++ app/models/course.rb
@@ -80,7 +80,6 @@ class Course < ApplicationRecord
 
   # Sync assignments for the course
   def self.sync_assignments(course_to_lms, token)
-    Rails.logger.info "Syncing assignments for course: #{course_to_lms.external_course_id}"
     # Fetch assignments from Canvas
     assignments = course_to_lms.fetch_assignments(token)
 
@@ -100,11 +99,9 @@ class Course < ApplicationRecord
 
   # Sync a single assignment
   def self.sync_assignment(course_to_lms, assignment_data)
-    # Rails.logger.info "#assignment_data: #{assignment_data}"
     assignment = Assignment.find_or_initialize_by(course_to_lms_id: course_to_lms.id, external_assignment_id: assignment_data['id'])
     assignment.name = assignment_data['name']
     assignment.due_date = DateTime.parse(assignment_data['due_at']) if assignment_data['due_at'].present?
-    # Rails.logger.info "Assignment due date: #{assignment.due_date}"
     assignment.late_due_date = DateTime.parse(assignment_data['lock_at']) if assignment_data['lock_at'].present? && (assignment.late_due_date.nil? || assignment.late_due_date < DateTime.parse(assignment_data['lock_at']))
     assignment.save!
   end
