4fc47546c4f6801169ea9d59d8b3528013ca4c06
Eric Huang <huanger@berkeley.edu>
2025-04-20 13:01:42 -0700

    updated default course_settings when importing a course

--- app/models/course.rb
+++ app/models/course.rb
@@ -58,6 +58,35 @@ class Course < ApplicationRecord
       form_setting.save!
     end
 
+    # Create a 1-to-1 course_settings record if it doesn't exist
+    unless course.course_settings
+      course_settings = course.build_course_settings(
+        enable_extensions: false,
+        auto_approve_days: 0,
+        auto_approve_dsp_days: 0,
+        max_auto_approve: 0,
+        enable_emails: false,
+        reply_email: nil,
+        email_subject: 'Extension Request Status: {{status}} - {{course_code}}',
+        email_template: <<~TEMPLATE
+          Dear {{student_name}},
+
+          Your extension request for {{assignment_name}} in {{course_name}} ({{course_code}}) has been {{status}}.
+
+          Extension Details:
+          - Original Due Date: {{original_due_date}}
+          - New Due Date: {{new_due_date}}
+          - Extension Days: {{extension_days}}
+
+          If you have any questions, please contact the course staff.
+
+          Best regards,
+          {{course_name}} Staff
+        TEMPLATE
+      )
+      course_settings.save!
+    end
+
     sync_assignments(course_to_lms, token)
     course.sync_enrollments_from_canvas(token)
     course
