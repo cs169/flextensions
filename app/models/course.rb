8cfb13b47a477f49d31c02f7327eec36d152d62c
Eric Huang <huanger@berkeley.edu>
2025-04-21 19:14:16 -0700

    refactored course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -26,66 +26,16 @@ class Course < ApplicationRecord
 
   # Fetch courses from Canvas API
   def self.fetch_courses(token)
-    response = Faraday.get("#{ENV.fetch('CANVAS_URL', nil)}/api/v1/courses") do |req|
-      req.headers['Authorization'] = "Bearer #{token}"
-      req.headers['Content-Type'] = 'application/json'
-      req.params['include[]'] = 'enrollment_type'
-    end
-
-    if response.success?
-      JSON.parse(response.body)
-    else
-      Rails.logger.error "Failed to fetch courses from Canvas: #{response.status} - #{response.body}"
-      []
-    end
+    make_canvas_request("#{ENV.fetch('CANVAS_URL')}/api/v1/courses", token, { 'include[]' => 'enrollment_type' })
   end
 
   # Create or find a course and its associated CourseToLms and assignments
   def self.create_or_update_from_canvas(course_data, token, _user)
-    course = find_or_create_course(course_data)
+    course = find_or_create_course(course_data, token)
     course_to_lms = find_or_create_course_to_lms(course, course_data)
 
-    # Creating a 1 to 1 form_settings record to course since the instructor is only meant to update form_settings
-    unless course.form_setting
-      form_setting = course.build_form_setting(
-        documentation_desc: <<~DESC,
-          Please provide links to any additional details if relevant.
-        DESC
-        documentation_disp: 'hidden',
-        custom_q1_disp: 'hidden',
-        custom_q2_disp: 'hidden'
-      )
-      form_setting.save!
-    end
-
-    # Create a 1-to-1 course_settings record if it doesn't exist
-    unless course.course_settings
-      course_settings = course.build_course_settings(
-        enable_extensions: false,
-        auto_approve_days: 0,
-        auto_approve_dsp_days: 0,
-        max_auto_approve: 0,
-        enable_emails: false,
-        reply_email: nil,
-        email_subject: 'Extension Request Status: {{status}} - {{course_code}}',
-        email_template: <<~TEMPLATE
-          Dear {{student_name}},
-
-          Your extension request for {{assignment_name}} in {{course_name}} ({{course_code}}) has been {{status}}.
-
-          Extension Details:
-          - Original Due Date: {{original_due_date}}
-          - New Due Date: {{new_due_date}}
-          - Extension Days: {{extension_days}}
-
-          If you have any questions, please contact the course staff.
-
-          Best regards,
-          {{course_name}} Staff
-        TEMPLATE
-      )
-      course_settings.save!
-    end
+    course.create_default_form_setting unless course.form_setting
+    course.create_default_course_settings unless course.course_settings
 
     sync_assignments(course_to_lms, token)
     course.sync_enrollments_from_canvas(token)
@@ -93,22 +43,14 @@ class Course < ApplicationRecord
   end
 
   # Find or create the course
-  def self.find_or_create_course(course_data)
+  def self.find_or_create_course(course_data, token)
     canvas_id = course_data['id']
-    response = Faraday.get("#{ENV.fetch('CANVAS_URL')}/api/v1/courses/#{canvas_id}") do |req|
-      req.headers['Authorization'] = "Bearer #{ENV.fetch('CANVAS_API_TOKEN')}" # Replace with the token variable
-      req.headers['Content-Type'] = 'application/json'
-    end
+    canvas_course = make_canvas_request("#{ENV.fetch('CANVAS_URL')}/api/v1/courses/#{canvas_id}", token)
 
-    if response.success?
-      canvas_course = JSON.parse(response.body)
-      course_name = canvas_course['name']
-      course_code = canvas_course['course_code']
-    else
-      Rails.logger.error "Failed to fetch course details from Canvas: #{response.status} - #{response.body}"
-      course_name = course_data['name']
-      course_code = course_data['course_code']
-    end
+    course = find_or_initialize_by(canvas_id: canvas_id)
+    course.course_name = canvas_course['name'] || course_data['name']
+    course.course_code = canvas_course['course_code'] || course_data['course_code']
+    course.save!
     course
   end
 
@@ -122,18 +64,11 @@ class Course < ApplicationRecord
 
   # Sync assignments for the course
   def self.sync_assignments(course_to_lms, token)
-    # Fetch assignments from Canvas
     assignments = course_to_lms.fetch_assignments(token)
-
-    # Keep track of external assignment IDs from Canvas
     external_assignment_ids = assignments.pluck('id')
 
-    # Sync or update assignments
-    assignments.each do |assignment_data|
-      sync_assignment(course_to_lms, assignment_data)
-    end
+    assignments.each { |assignment_data| sync_assignment(course_to_lms, assignment_data) }
 
-    # Delete assignments that no longer exist in Canvas
     Assignment.where(course_to_lms_id: course_to_lms.id)
               .where.not(external_assignment_id: external_assignment_ids)
               .destroy_all
@@ -143,79 +78,102 @@ class Course < ApplicationRecord
   def self.sync_assignment(course_to_lms, assignment_data)
     assignment = Assignment.find_or_initialize_by(course_to_lms_id: course_to_lms.id, external_assignment_id: assignment_data['id'])
     assignment.name = assignment_data['name']
-
-    # Extract due_at from base_date if present
-    if assignment_data['base_date'] && assignment_data['base_date']['due_at'].present?
-      assignment.due_date = DateTime.parse(assignment_data['base_date']['due_at'])
-    elsif assignment_data['due_at'].present?
-      assignment.due_date = DateTime.parse(assignment_data['due_at'])
-    end
-
-    # Extract lock_at for late_due_date
-    if assignment_data['base_date'] && assignment_data['base_date']['lock_at'].present?
-      assignment.late_due_date = DateTime.parse(assignment_data['base_date']['lock_at'])
-    elsif assignment_data['lock_at'].present?
-      assignment.late_due_date = DateTime.parse(assignment_data['lock_at'])
-    end
-
+    assignment.due_date = parse_canvas_date(assignment_data, 'due_at')
+    assignment.late_due_date = parse_canvas_date(assignment_data, 'lock_at')
     assignment.save!
   end
 
   # Fetch users for a course from Canvas API
   def fetch_users_from_canvas(token, enrollment_type = nil)
-    url = "#{ENV.fetch('CANVAS_URL')}/api/v1/courses/#{canvas_id}/users"
-    response = Faraday.get(url) do |req|
-      req.headers['Authorization'] = "Bearer #{token}"
-      req.headers['Content-Type'] = 'application/json'
-      req.params['enrollment_type'] = enrollment_type if enrollment_type.present?
-    end
-
-    if response.success?
-      JSON.parse(response.body)
-    else
-      Rails.logger.error "Failed to fetch users from Canvas: #{response.status} - #{response.body}"
-      []
-    end
+    make_canvas_request("#{ENV.fetch('CANVAS_URL')}/api/v1/courses/#{canvas_id}/users", token, { 'enrollment_type' => enrollment_type })
   end
 
   # Fetch users for a course and create/find their User and UserToCourse records
   def sync_users_from_canvas(token, role)
-    # Fetch all users for the course from Canvas
     users_data = fetch_users_from_canvas(token, role)
-
-    # Extract the Canvas user IDs from the fetched data
     current_canvas_user_ids = users_data.pluck('id')
 
-    # Find all UserToCourse records for this course and role
-    existing_user_to_courses = UserToCourse.where(course_id: id, role: role)
-
     # Delete UserToCourse records for users no longer in the course
-    existing_user_to_courses.each do |user_to_course|
+    UserToCourse.where(course_id: id, role: role).find_each do |user_to_course|
       user_to_course.destroy unless current_canvas_user_ids.include?(user_to_course.user.canvas_uid)
     end
 
     # Create or update User and UserToCourse records for current users
     users_data.each do |user_data|
-      # this line skips importing a user if the api doesn't return their email
-      # one case this is happening if the user was invited to the course but hasn't accepted
       next if user_data['email'].blank?
 
-      # Create or find the User model
       user = User.find_or_create_by(canvas_uid: user_data['id']) do |u|
         u.name = user_data['name']
-        u.email = user_data['email'] # Assuming login_id is the email
+        u.email = user_data['email']
       end
       user.student_id = user_data['sis_user_id']
       user.save!
 
-      # Create or update the UserToCourse record
       UserToCourse.find_or_create_by(user_id: user.id, course_id: id, role: role)
     end
   end
 
   def sync_enrollments_from_canvas(token)
-    sync_users_from_canvas(token, 'student')
-    sync_users_from_canvas(token, 'teacher')
-    sync_users_from_canvas(token, 'ta')
+    %w[student teacher ta].each { |role| sync_users_from_canvas(token, role) }
+  end
+
+  # Helper methods
+  def self.make_canvas_request(url, token, params = {})
+    response = Faraday.get(url) do |req|
+      req.headers['Authorization'] = "Bearer #{token}"
+      req.headers['Content-Type'] = 'application/json'
+      params.each { |key, value| req.params[key] = value if value.present? }
+    end
+
+    if response.success?
+      JSON.parse(response.body)
+    else
+      Rails.logger.error "Failed to fetch data from Canvas: #{response.status} - #{response.body}"
+      []
+    end
+  end
+
+  def self.parse_canvas_date(data, key)
+    if data['base_date'] && data['base_date'][key].present?
+      DateTime.parse(data['base_date'][key])
+    elsif data[key].present?
+      DateTime.parse(data[key])
+    end
+  end
+
+  def create_default_form_setting
+    build_form_setting(
+      documentation_desc: 'Please provide links to any additional details if relevant.',
+      documentation_disp: 'hidden',
+      custom_q1_disp: 'hidden',
+      custom_q2_disp: 'hidden'
+    ).save!
+  end
+
+  def create_default_course_settings
+    build_course_settings(
+      enable_extensions: false,
+      auto_approve_days: 0,
+      auto_approve_dsp_days: 0,
+      max_auto_approve: 0,
+      enable_emails: false,
+      reply_email: nil,
+      email_subject: 'Extension Request Status: {{status}} - {{course_code}}',
+      email_template: <<~TEMPLATE
+        Dear {{student_name}},
+
+        Your extension request for {{assignment_name}} in {{course_name}} ({{course_code}}) has been {{status}}.
+
+        Extension Details:
+        - Original Due Date: {{original_due_date}}
+        - New Due Date: {{new_due_date}}
+        - Extension Days: {{extension_days}}
+
+        If you have any questions, please contact the course staff.
+
+        Best regards,
+        {{course_name}} Staff
+      TEMPLATE
+    ).save!
   end
 end
