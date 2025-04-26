9a2a00535d47008587a9034f9f58546bb25fcb43
Eric Huang <huanger@berkeley.edu>
2025-04-26 14:52:29 -0700

    attempt 2 to implement datatables

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,22 @@
 import "assignments"
 import "controllers"
 import "rails-ujs-override"
+
+// Import jQuery and DataTables
+import "jquery";
+import "datatables.net";
+import "datatables.net-dt";
+
+// Initialize DataTables
+document.addEventListener("DOMContentLoaded", () => {
+  $('#requests-table').DataTable({
+    paging: true,
+    searching: true,
+    ordering: true,
+    info: true,
+    columnDefs: [
+      { orderable: false, targets: "no-sort" } // Disable sorting for columns with the "no-sort" class
+    ],
+    order: [[3, "asc"]] // Default sort by the "Requested At" column in ascending order
+  });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,8 @@ pin "rails-ujs-override", to: "rails-ujs-override.js"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "assignments", to: "assignments.js"
+
+# Pin DataTables and jQuery
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "datatables.net", to: "https://ga.jspm.io/npm:datatables.net@1.13.6/js/jquery.dataTables.js"
+pin "datatables.net-dt", to: "https://ga.jspm.io/npm:datatables.net-dt@1.13.6/css/jquery.dataTables.css"
