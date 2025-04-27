833521f59287c7eeb6dae754f382c22803a483ae
Eric Huang <huanger@berkeley.edu>
2025-04-26 20:15:49 -0700

    add requests controller js

--- app/javascript/controllers/requests_controller.js
+++ app/javascript/controllers/requests_controller.js
@@ -1,13 +1,16 @@
-// Initialize DataTables
-document.addEventListener("DOMContentLoaded", () => {
-    $('#requests-table').DataTable({
-      paging: true,
-      searching: true,
-      ordering: true,
-      info: true,
-      columnDefs: [
-        { orderable: false, targets: "no-sort" } // Disable sorting for columns with the "no-sort" class
-      ],
-      order: [[3, "asc"]] // Default sort by the "Requested At" column in ascending order
-    });
-  });
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    connect() {
+        $('#requests-table').DataTable({
+            paging: true,
+            searching: true,
+            ordering: true,
+            info: true,
+            columnDefs: [
+                { orderable: false, targets: "no-sort" } // Disable sorting for columns with the "no-sort" class
+            ],
+            order: [[3, "asc"]] // Default sort by the "Requested At" column in ascending order
+        });
+    }
+}
