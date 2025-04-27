3373d6e026f69676a82ed44186862560839ace90
Eric Huang <huanger@berkeley.edu>
2025-04-26 21:03:14 -0700

    make all date cols sorted by date

--- app/javascript/controllers/requests_controller.js
+++ app/javascript/controllers/requests_controller.js
@@ -10,9 +10,9 @@ export default class extends Controller {
                 info: true,
                 columnDefs: [
                     { orderable: false, targets: "no-sort" }, // Disable sorting for columns with the "no-sort" class
-                    { type: "date", targets: 3 } // Ensure the "Requested At" column (index 3) is sorted by date
+                    { type: "date", targets: [3, 4, 5] } // Ensure "Requested At", "Original Due Date", and "Requested Due Date" are sorted by date
                 ],
-                order: [[3, "asc"]]
+                order: [[3, "asc"]] // Default sort by the "Requested At" column in ascending order
             });
         }
     }
