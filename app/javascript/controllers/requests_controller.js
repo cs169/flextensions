f39a7bf0c3cbe8b76091ce3916795bd0a131fc4c
Eric Huang <huanger@berkeley.edu>
2025-04-26 20:18:53 -0700

    test data table

--- app/javascript/controllers/requests_controller.js
+++ app/javascript/controllers/requests_controller.js
@@ -2,15 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
     connect() {
-        $('#requests-table').DataTable({
-            paging: true,
-            searching: true,
-            ordering: true,
-            info: true,
-            columnDefs: [
-                { orderable: false, targets: "no-sort" } // Disable sorting for columns with the "no-sort" class
-            ],
-            order: [[3, "asc"]] // Default sort by the "Requested At" column in ascending order
-        });
+        if (!$.fn.DataTable.isDataTable('#requests-table')) {
+            $('#requests-table').DataTable({
+                paging: true,
+                searching: true,
+                ordering: true,
+                info: true,
+                columnDefs: [
+                    { orderable: false, targets: "no-sort" } // Disable sorting for columns with the "no-sort" class
+                ],
+                order: [[3, "desc"]] // Default sort by the "Requested At" column in ascending order
+            });
+        }
     }
 }
