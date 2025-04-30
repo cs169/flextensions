3f975d120c440dae2cfdf90875acdbedaeddc1e7
Eric Huang <huanger@berkeley.edu>
2025-04-29 17:22:08 -0700

    debug role

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,13 +1,12 @@
 class AssignmentsController < ApplicationController
   def toggle_enabled
-    unless %w[teacher ta].include?(@role)
-      flash[:alert] = 'You do not have permission to perform this action.'
-      return render json: { redirect_to: course_path(params[:course_id]) }, status: :forbidden
-    end
-
     @assignment = Assignment.find(params[:id])
     course = @assignment.course_to_lms.course
-
+    unless %w[teacher ta].include?(@role)
+      Rails.logger.error "Role #{@role} does not have permission to toggle assignment enabled status"
+      flash[:alert] = 'You do not have permission to perform this action.'
+      return render json: { redirect_to: course_path(course) }, status: :forbidden
+    end
     if @assignment.update(enabled: params[:enabled])
       render json: { success: true }, status: :ok
     else
