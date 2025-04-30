c8aa97363b25820692143ee670122ab5fffa7e2a
Eric Huang <huanger@berkeley.edu>
2025-04-29 17:38:24 -0700

    rubocop and instructor only

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -5,9 +5,9 @@ class AssignmentsController < ApplicationController
     @role = params[:role] || course&.user_role(@user) # Use the passed role or fallback to determining it
     @user = User.find(params[:user_id]) if params[:user_id] # Optionally find the user if user_id is passed
 
-    unless %w[teacher ta].include?(@role)
+    unless @role == 'instructor'
       Rails.logger.error "Role #{@role} does not have permission to toggle assignment enabled status"
-      flash[:alert] = 'You do not have permission to perform this action.'
+      flash.now[:alert] = 'You do not have permission to perform this action.'
       return render json: { redirect_to: course_path(course) }, status: :forbidden
     end
 
