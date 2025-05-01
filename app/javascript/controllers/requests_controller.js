95d0820bb9a7e868f3fdfe30e12b1d56573f1d98
Yaman Tarakji <yamyoume@hotmail.com>
2025-05-01 00:11:50 -0700

    dataTables now bootstrap5 and enhanced table styles a bit

--- app/javascript/controllers/requests_controller.js
+++ app/javascript/controllers/requests_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
-import DataTable from "datatables.net";
+import DataTable from "datatables.net-bs5";
+import "datatables.net-responsive";
+import "datatables.net-responsive-bs5";
 
 export default class extends Controller {
     connect() {
@@ -11,6 +13,7 @@ export default class extends Controller {
                 searching: true,
                 ordering: true,
                 info: true,
+                // responsive: true,
                 columnDefs: [
                     { orderable: false, targets: 'no-sort' }, // Disable sorting for columns with the "no-sort" class
                     { type: "date", targets: [3, 4, 5] } // Ensure "Requested At", "Original Due Date", and "Requested Due Date" are sorted by date
