1c3001b9506b1132e8a80faa46dcb37dfa553d64
Yaman Tarakji <yamyoume@hotmail.com>
2025-05-08 16:50:21 -0700

    a11y test fix attempt 1

--- spec/factories/lms.rb
+++ spec/factories/lms.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :lms do
     id { 1 } # Explicitly set id to 1 (must be hardcoded)
-    lms_name { 'canvas' }
+    lms_name { 'Canvas' }
     use_auth_token { true }
   end
 end
