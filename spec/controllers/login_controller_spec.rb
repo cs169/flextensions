0b31c0e3181444db76a97af67d452c06812eaf78
Michael Ball <michael@mball.co>
2025-08-12 02:07:10 -0700

    soft deprecate login controller for now

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
-  let(:encoded_scopes) { CGI.escape(CanvasFacade::CANVAS_API_SCOPES) }
+  # TODO: Remove login controller and move logout to session controller.
+  # let(:encoded_scopes) { CGI.escape(CanvasFacade::CANVAS_API_SCOPES) }
 
-  describe 'GET #canvas' do
-    it 'redirects to canvas authorization URL' do
-      allow(ENV).to receive(:fetch).with('CANVAS_CLIENT_ID', nil).and_return('test_client_id')
-      allow(ENV).to receive(:fetch).with('CANVAS_REDIRECT_URI', nil).and_return('http://localhost:3000')
-      allow(ENV).to receive(:fetch).with('CANVAS_URL', nil).and_return('https://ucberkeleysandbox.instructure.com')
+  # describe 'GET #canvas' do
+  #   it 'redirects to canvas authorization URL' do
+  #     allow(ENV).to receive(:fetch).with('CANVAS_CLIENT_ID', nil).and_return('test_client_id')
+  #     allow(ENV).to receive(:fetch).with('CANVAS_REDIRECT_URI', nil).and_return('http://localhost:3000')
+  #     allow(ENV).to receive(:fetch).with('CANVAS_URL', nil).and_return('https://ucberkeleysandbox.instructure.com')
 
-      get :canvas
+  #     get :canvas
 
-      expected_url = 'https://ucberkeleysandbox.instructure.com/login/oauth2/auth?'
-      expect(response).to redirect_to(/^#{Regexp.escape(expected_url)}/)
-      expect(response.location).to include('client_id=test_client_id')
-      expect(response.location).to include('response_type=code')
-      expect(response.location).to include('redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcanvas%2Fcallback')
-      expect(response.location).to include(encoded_scopes)
-    end
-  end
+  #     expected_url = 'https://ucberkeleysandbox.instructure.com/login/oauth2/auth?'
+  #     expect(response).to redirect_to(/^#{Regexp.escape(expected_url)}/)
+  #     expect(response.location).to include('client_id=test_client_id')
+  #     expect(response.location).to include('response_type=code')
+  #     expect(response.location).to include('redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcanvas%2Fcallback')
+  #     expect(response.location).to include(encoded_scopes)
+  #   end
+  # end
 
   describe 'GET #logout' do
     before do
@@ -34,19 +35,19 @@ RSpec.describe LoginController, type: :controller do
     end
   end
 
-  describe '#canvas_authorize_url' do
-    it 'builds the correct canvas authorization URL' do
-      allow(ENV).to receive(:fetch).with('CANVAS_CLIENT_ID', nil).and_return('test_client_id')
-      allow(ENV).to receive(:fetch).with('CANVAS_REDIRECT_URI', nil).and_return('http://localhost:3000')
-      allow(ENV).to receive(:fetch).with('CANVAS_URL', nil).and_return('https://ucberkeleysandbox.instructure.com')
+  # describe '#canvas_authorize_url' do
+  #   it 'builds the correct canvas authorization URL' do
+  #     allow(ENV).to receive(:fetch).with('CANVAS_CLIENT_ID', nil).and_return('test_client_id')
+  #     allow(ENV).to receive(:fetch).with('CANVAS_REDIRECT_URI', nil).and_return('http://localhost:3000')
+  #     allow(ENV).to receive(:fetch).with('CANVAS_URL', nil).and_return('https://ucberkeleysandbox.instructure.com')
 
-      url = controller.send(:canvas_authorize_url) # send to test private method
+  #     url = controller.send(:canvas_authorize_url) # send to test private method
 
-      expect(url).to start_with('https://ucberkeleysandbox.instructure.com/login/oauth2/auth?')
-      expect(url).to include('client_id=test_client_id')
-      expect(url).to include('response_type=code')
-      expect(url).to include('redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcanvas%2Fcallback')
-      expect(url).to include(encoded_scopes)
-    end
-  end
+  #     expect(url).to start_with('https://ucberkeleysandbox.instructure.com/login/oauth2/auth?')
+  #     expect(url).to include('client_id=test_client_id')
+  #     expect(url).to include('response_type=code')
+  #     expect(url).to include('redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcanvas%2Fcallback')
+  #     expect(url).to include(encoded_scopes)
+  #   end
+  # end
 end
