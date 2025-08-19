4f509d136c78dc9a9c19a023d15b37b2a7e69531
Michael Ball <michael@mball.co>
2025-08-19 14:42:36 -0700

    delint

--- spec/Facades/lms_facade_spec.rb
+++ spec/Facades/lms_facade_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 describe LmsFacade do
   let(:facade) { described_class.new }
-  let(:mockCourse) { 16 }
-  let(:mockStudent) { 22 }
-  let(:mockAssignment) { 18 }
-  let(:mockDate) { '2002-03-16:16:00:00Z' }
+  let(:mock_course) { 16 }
+  let(:mock_student) { 22 }
+  let(:mock_assignment) { 18 }
+  let(:mock_date) { '2002-03-16:16:00:00Z' }
 
   describe 'provision_extension' do
     it 'throws not implemented error' do
       expect do
         facade.provision_extension(
-          mockCourse,
-          mockStudent,
-          mockAssignment,
-          mockDate
+          mock_course,
+          mock_student,
+          mock_assignment,
+          mock_date
         )
       end.to raise_error(NotImplementedError)
     end
