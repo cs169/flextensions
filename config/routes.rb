d5859764db1faf9242c7caa209c912037747ac66
Michael Ball <michael@mball.co>
2025-08-21 16:13:42 -0700

    Basic health check and version endpoints, including specs

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'status/health_check', to: 'status#health_check'
+  get 'status/version', to: 'status#version'
 
   root "home#index"
   get '/courses', to: 'courses#index', as: 'courses'
