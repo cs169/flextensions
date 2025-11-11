31aa00088c39f5b64ad09394639a4af2fbffa478
Michael Ball <michael@mball.co>
2025-11-11 15:51:52 -0800

    Do not load the LMS table in an initializer

--- config/initializers/lms_integrations.rb
+++ config/initializers/lms_integrations.rb
@@ -4,13 +4,13 @@ Rails.application.config.after_initialize do
   CANVAS_LMS_ID = 1
   GRADESCOPE_LMS_ID = 2
 
-  begin
-    next unless ActiveRecord::Base.connection.table_exists?('lmss')
-    # Warm the tiny cache.
-    Lms.CANVAS_LMS
-    Lms.GRADESCOPE_LMS
-  rescue ActiveRecord::NoDatabaseError
-    # Skip if database doesn't exist
-    next
-  end
+  # begin
+  #   next unless ActiveRecord::Base.connection.table_exists?('lmss')
+  #   # Warm the tiny cache.
+  #   Lms.CANVAS_LMS
+  #   Lms.GRADESCOPE_LMS
+  # rescue ActiveRecord::NoDatabaseError
+  #   # Skip if database doesn't exist
+  #   next
+  # end
 end
