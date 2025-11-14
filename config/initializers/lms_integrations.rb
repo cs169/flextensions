49e874fc980405f250835bee007e5424f52e3aa2
Michael Ball <michael@mball.co>
2025-11-14 00:31:01 -0800

    Try to improve default logging config

--- config/initializers/lms_integrations.rb
+++ config/initializers/lms_integrations.rb
@@ -1,16 +1,7 @@
 # Ensure all LMS integrations are set up
-
+# TODO: This no longer ensures all LMS integrations are set up.
+# Updating `Lms` here breaks when there are migrations pending.
 Rails.application.config.after_initialize do
   CANVAS_LMS_ID = 1
   GRADESCOPE_LMS_ID = 2
-
-  # begin
-  #   next unless ActiveRecord::Base.connection.table_exists?('lmss')
-  #   # Warm the tiny cache.
-  #   Lms.CANVAS_LMS
-  #   Lms.GRADESCOPE_LMS
-  # rescue ActiveRecord::NoDatabaseError
-  #   # Skip if database doesn't exist
-  #   next
-  # end
 end
