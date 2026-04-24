404b44982345fba537ca8739da8a10ab07de8f51
Anthony Dam <anthonydam@berkeley.edu>
2026-04-23 19:05:33 -0700

    Restore filter_by_semester method lost during merge

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -133,6 +133,10 @@ class CoursesController < ApplicationController
     sorted_semesters.map { |semester| [ semester, grouped[semester] ] }
   end
 
+  def filter_by_semester(courses, semester)
+    courses.select { |c| c.dig('term', 'name') == semester }
+  end
+
   # TODO: This should be moved to the Canvas Facade
   def filter_courses(courses, roles, exclude_ids = [])
     missing_enrollments = courses.select { |course| course['enrollments'].blank? }
